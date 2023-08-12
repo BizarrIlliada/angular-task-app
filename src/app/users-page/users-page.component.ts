714e0bbee8934b0dd11522d7c38f633346939aba
BizarrIlliada <kazkoviy@icloud.com>
2023-08-12 20:56:24 +0300

    sorting was added

--- src/app/users-page/users-page.component.ts
+++ src/app/users-page/users-page.component.ts
@@ -15,16 +15,17 @@ interface User {
   styleUrls: ['./users-page.component.scss']
 })
 export class UsersPageComponent {
-
-  loadedUsers: {} = {};
+  loadedUsers: User[] = [];
   listOfUsers: User[] = [];
+  nameSortParam: 'asc' | 'desc' = 'desc';
+  dateSortParam: 'asc' | 'desc' = 'asc';
 
   constructor(private requestUsers: RequestsUser, private router: Router) {};
 
   ngOnInit() {
     this.requestUsers.getUsers().subscribe((users: Record<string, any>) => {
       for (const id in users) {
-        this.listOfUsers.push({
+        this.loadedUsers.push({
           id,
           name: users[id].name,
           date: users[id].date,
@@ -32,5 +33,36 @@ export class UsersPageComponent {
         })
       }
     });
+
+    this.listOfUsers = this.loadedUsers;
+  }
+
+  onSortName() {
+    if (this.nameSortParam === 'desc') {
+      this.listOfUsers.sort((a, b) => a.name.localeCompare(b.name));
+      this.nameSortParam = 'asc';
+    } else {
+      this.listOfUsers.reverse();
+      this.nameSortParam = 'desc';
+    }
+
+    this.dateSortParam = 'asc';
+  }
+
+  onSortDate() {
+    if (this.dateSortParam === 'asc') {
+      this.listOfUsers.sort((a, b) => {
+        const dateA = new Date(a.date).getTime();
+        const dateB = new Date(b.date).getTime();
+
+        return dateA - dateB;
+      });
+      this.dateSortParam = 'desc';
+    } else {
+      this.listOfUsers.reverse();
+      this.dateSortParam = 'asc';
+    }
+
+    this.nameSortParam = 'desc';
   }
 }
